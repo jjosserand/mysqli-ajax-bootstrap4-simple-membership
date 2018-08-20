f35d0e909532f805ef47da0333997ae881aa43ba
Jesse Josserand <jesse.josserand@gmail.com>
2018-08-20 18:47:39 -0400

    Updated conn.php
    Added comments to explain use of conn.php within the file.

--- conn.php
+++ conn.php
@@ -1,8 +1,10 @@
 <?php
+//leave servername set to localhost -- if you have problems, contact your hosting company for correct setting
 $servername = "localhost";
-$dbname = "sqpgclub_reg";
-$username = "sqpgclub_regadm";
-$password = "Solarflare2018##";
+//create a database, then change items in quotes to the correct settings for your database
+$dbname = "cpaneladm_databasename";
+$username = "cpaneladm_databaseuser";
+$password = "yoursecurepasswordhere";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
